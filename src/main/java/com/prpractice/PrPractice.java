c9881722d1ab3adb6519b7d99ef7d73a3e2fdf31
Stephen Squire <88947471+Genoson@users.noreply.github.com>
2022-06-06 13:40:33 -0230

    SSquire-practice-code-added

---
+++ src/main/java/com/prpractice/PrPractice.java
@@ -0,0 +1,15 @@
+package com.prpractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrPractice {
+
+    private List<String> names = new ArrayList<String>();
+
+    public List<String> populateNames() {
+        names.add("Stephen Squire");
+        return names;
+    }
+    
+}
