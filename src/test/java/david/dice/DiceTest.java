e07f03c90d94725083bfb4fd501999d503b11ad6
DavidJTemple <96357555+DavidJTemple@users.noreply.github.com>
2022-06-08 22:40:46 -0230

    DavesCoolBranchDoritos

--- src/test/java/david/dice/DiceTest.java
+++ src/test/java/david/dice/DiceTest.java
@@ -16,8 +16,16 @@ public class DiceTest {
     public void definiteTwoDieRoll(){
         Dice myDice = new Dice(1, 6);
         Assertions.assertEquals(myDice.getTotal() > 6, myDice.getTotal() > 6);
-        System.out.println("the total of your roll was " + myDice.getTotal());
+        System.out.println("The total of your roll was " + myDice.getTotal());
         System.out.println("If the total is greater than 6, more than one dice was rolled");
 
+    }
+
+    @Test
+    public void aRollWasMade(){
+        Dice myDice = new Dice(4, 3);
+        Assertions.assertNotNull(myDice.getTotal());
+        System.out.println("The total of your dice roll was " + myDice.getTotal());
+        System.out.println("");
     }
 }
